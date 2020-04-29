c60b436ee1767b930de182b4e3124251bc81e774
Deryana <deredhuzent@gmail.com>
2020-04-28 21:28:06 -0700

    characterController movement
    some names of the code fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerController : MonoBehaviour {
+	[SerializeField] float speed = 4f;
+    Vector3 forward, right, move, horizontalMovement, verticalMovement, direction;
+	
+	private void Start()
+	{
+        //isometric view
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward = Vector3.Normalize(forward);
+        //90 rotation
+		right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Update()
+	{
+		if(Input.anyKey) MoveCharacter();
+	}
+	
+	private void MoveCharacter()
+	{
+        //inputs:
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+		move = new Vector3(x, 0, z);
+		horizontalMovement = right * speed * Time.deltaTime * x;
+		verticalMovement = forward * speed * Time.deltaTime * z;
+		
+		direction = Vector3.Normalize(horizontalMovement + verticalMovement);
+		
+		transform.forward = direction;
+		transform.position += horizontalMovement;
+		transform.position += verticalMovement;
+	}
 }
