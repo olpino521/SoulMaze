f86c7428c5e7f863611c7b96bd2e6398b159652e
Deryana <deredhuzent@gmail.com>
2020-05-02 16:14:29 -0700

    Basic Spawning
    -SpawnItems -> Items spawn if a button is clicked
    -Folders creation (Items, Enemies, Camera)
    -changed names of prefabs soul and box

---
+++ Assets/Scripts/Items/Items.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    [SerializeField] GameObject soul, box;
+    //TODO: if is breakable or pickup
+    //TODO: collition on trigger
+    //TODO: counter of pickups
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Items/SpawnItems.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+    [SerializeField] private GameObject souls, box;
+    Vector3 location;
+
+    List<GameObject> objectsSpawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initiliaze list
+        objectsSpawned = new List<GameObject>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawn();
+        
+    }
+
+    void spawn(){
+
+        location = new Vector3(Random.Range(-9,9), 1, Random.Range(-14,14));
+
+        //TODO: don't instantiate where there is a object
+
+      
+        //Keys: 1 - souls & 2 - Box
+        spawnByClick();
+    }
+
+    void spawnByClick(){
+
+        if(Input.GetKeyDown("1")) {
+
+          Instantiate(souls, location, transform.rotation);
+
+        } else if(Input.GetKeyDown("2")){
+      
+            Instantiate(box, location, transform.rotation);
+            
+        }
+    }
+}
